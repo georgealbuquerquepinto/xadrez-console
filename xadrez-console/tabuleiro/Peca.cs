c365d98a2a68a8ac655e01eee4ee63e9256cee07
George Albuquerque Pinto <georgealbuquerquepinto@hotmail.com>
2020-10-08 14:00:22 -0300

    Inicia implemetação do tabuleiro

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    public class Peca
+    {
+        public Peca(Posicao posicao, Cor cor, int qtdMovimentos, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            this.cor = cor;
+            this.qtdMovimentos = qtdMovimentos;
+            this.tab = tab;
+        }
+
+        public Posicao Posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+    }
+}
